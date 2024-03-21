ce5733f2f763a68cfa7cc6e98f63282dc7bde290
Florent Uzio <fuzio@ripple.com>
2024-03-21 14:14:44 +0400

    xrpl

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { XrplModule } from './xrpl/xrpl.module';
 
 @Module({
-  imports: [],
+  imports: [XrplModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
