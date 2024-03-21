1900b9b645656c3eb192a201f88812960f98021f
Florent Uzio <fuzio@ripple.com>
2024-03-21 14:18:39 +0400

    xrp service

--- src/xrpl/xrpl.service.ts
+++ src/xrpl/xrpl.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Client } from 'xrpl';
+
+const XRPL_LABS_TESTNET = 'wss://testnet.xrpl-labs.com/';
 
 @Injectable()
-export class XrplService {}
+export class XrplService extends Client implements OnModuleInit {
+  private readonly logger = new Logger(XrplService.name);
+
+  constructor() {
+    super(XRPL_LABS_TESTNET);
+  }
+
+  async onModuleInit() {
+    await this.connect();
+
+    if (this.isConnected()) {
+      this.logger.log(`XRPL Service connected to ${this.url}`);
+    } else {
+      const errorMessage = `XRPL Service could not connect to ${this.url}`;
+      throw new Error(errorMessage);
+    }
+  }
+}
